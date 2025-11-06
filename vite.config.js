205ffe885f5e4f535aea639129d7b4f25160097a
anamguzmn26 <199285904+anamguzmn26@users.noreply.github.com>
2025-11-05 20:42:00 -0500

    feat: config vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mini-Project1-React', // 👈 importante: debe tener slash inicial y final
+  base: '/' // 👈 importante: debe tener slash inicial y final
 })
